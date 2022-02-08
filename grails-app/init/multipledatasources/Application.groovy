3172e8f7c1ab07c7c4f1b68291951cc7e9211971
Frank J Kocina <frank.j.kocina@gmail.com>
2022-02-08 09:56:36 -0600

    add references for getting osiv working

--- grails-app/init/multipledatasources/Application.groovy
+++ grails-app/init/multipledatasources/Application.groovy
@@ -11,6 +11,10 @@ class Application extends GrailsAutoConfiguration {
         GrailsApp.run(Application, args)
     }
 
+    // https://github.com/grails/grails-core/issues/10383
+    // https://github.com/grails/grails-data-mapping/blob/v5.0.12/grails-datastore-gorm-hibernate5/src/main/groovy/grails/orm/bootstrap/HibernateDatastoreSpringInitializer.groovy#L314
+    // https://github.com/grails/gorm-hibernate5/blob/v6.1.12/grails-datastore-gorm-hibernate5/src/main/groovy/grails/orm/bootstrap/HibernateDatastoreSpringInitializer.groovy#L203
+    // https://stackoverflow.com/questions/42443260/using-opensessioninviewinterceptor-in-java-config-not-webxml
     @Bean
     GrailsOpenSessionInViewInterceptor openSessionInViewInterceptor_books() {
         HibernateDatastore hibernateDatastore = applicationContext.getBean('hibernateDatastore') as HibernateDatastore
